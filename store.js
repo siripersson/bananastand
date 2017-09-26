34bb0ff3cb5fec24451631330702304a465db5f2
siripersson <siri.persson1@gmail.com>
2017-09-26 11:58:50 +0200

    Add counter

--- store.js
+++ store.js
@@ -1,7 +1,10 @@
+const bananasplit=50;
+const spoon=3;
 
 // Funktion
 const greet = () => {
-	console.log('Välkommen!')
+	const tot=2*bananasplit+ 5* spoon
+	console.log('Välkommen! '+tot + 'kr')
 }
 
 // Anropa funktionen
