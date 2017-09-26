71893766b3deeba3648a8c8fcdf4c2fc351ea178
siripersson <siri.persson1@gmail.com>
2017-09-26 12:13:31 +0200

    Add bananasplit and spoon

--- store.js
+++ store.js
@@ -9,3 +9,27 @@ const greet = () => {
 
 // Anropa funktionen
 greet()
+
+// Objekt
+bananasplit_obj= {
+	name_b: "banan",
+	price_b: 15
+}
+
+spoon_obj= {
+	name_s: "spoon",
+	price_s: 5
+}
+
+// Funktion som printar
+const describeBananasplit =() => {
+	console.log(bananasplit_obj.name_b + " with price: " + bananasplit_obj.price_b +'sek')
+}
+
+const describeSpoon =() => {
+	console.log(spoon_obj.name_s + " with price: " + spoon_obj.price_s + 'sek')
+}
+
+// Anropa
+describeBananasplit()
+describeSpoon()
